8f79ebb189d213bd4c3641d23aa56ac49a3f5be0
SLB974 <slb974run@gmail.com>
2019-06-29 04:35:13 +0400

    1er essai precommit

--- test1.py
+++ test1.py
@@ -1,1 +1,24 @@
 print("Oté Dalon !")
+coco = [
+    "papa",
+    "maman",
+    "christophe",
+    "stephane",
+    "vincent",
+    "maryrose",
+    "papy",
+    "mamie",
+    "pacha",
+    "dominique",
+    "michel",
+    "catherine",
+    "Brigitte",
+    "françoise",
+    "philippe",
+    "magalie",
+    "frédéric",
+    "nathalie",
+    "manon",
+    "violette",
+]
+paopopa
